c5748b521a4aa643f05b5c92f82fa8dad234f492
Ralph Perdomo <ralph@prdmo.com>
2018-12-07 09:32:10 -0800

    make 7 specs pass for Project class spec test

--- lib/project.rb
+++ lib/project.rb
@@ -7,4 +7,26 @@ class Project
 
   end
 
+  def self.all
+    all_projects_from_db = DB.exec("SELECT * FROM projects")
+    all_projects = []
+
+    all_projects_from_db.each do |project|
+      all_projects.push(Project.new({
+        title: project["title"],
+        id: project["id"].to_i
+        }))
+    end
+    all_projects
+  end
+
+  def ==(other_proj)
+    @title.==(other_proj.title) & @id.==(other_proj.id)
+
+  end
+
+  def save
+    @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first["id"].to_i
+  end
+
 end
