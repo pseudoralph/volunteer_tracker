7b52fd7dd5c7aa1b8e385f563fefd6da26b810a4
Ralph Perdomo <ralph@prdmo.com>
2018-12-07 09:09:00 -0800

    add blank sql file with tables

--- app.rb
+++ app.rb
@@ -2,13 +2,11 @@ require 'pg'
 require 'sinatra'
 require 'sinatra/reloader'
 also_reload './lib/**/*.rb'
-require_relative 'lib/volunteers.rb'
-require_relative 'lib/projects.rb'
+require_relative 'lib/volunteer.rb'
+require_relative 'lib/project.rb'
 
-
-# DB = PG.connect({dbname: 'volunteer_tracker'})
+DB = PG.connect({dbname: 'volunteer_tracker'})
 
 get '/' do
-
   erb :index
 end
